595aca3511ff2086797145bf0b1bc49474452f99
barakamulas <barakafrank18@gmail.com>
2019-11-10 18:49:36 +0300

    Create a spannable string to make the Sign up part of the SignUp text view in main to change its color to blue

--- app/src/main/java/com/baraka/thescout/MainActivity.java
+++ app/src/main/java/com/baraka/thescout/MainActivity.java
@@ -2,7 +2,12 @@ package com.baraka.thescout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView mAskSignUp = findViewById(R.id.account_question_main);
+        String text = "Don't have an account? Sign Up";
+        SpannableString ss = new SpannableString(text);
+        ForegroundColorSpan blue = new ForegroundColorSpan(Color.BLUE);
+        ss.setSpan(blue,23,30, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        mAskSignUp.setText(ss);
+
     }
 }
