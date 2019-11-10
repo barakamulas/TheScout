2e1daa245d1cf15fc77a5610c961c5102805e619
barakamulas <barakafrank18@gmail.com>
2019-11-10 22:32:23 +0300

    Refactor main activity using Butterknife

--- app/src/main/java/com/baraka/thescout/MainActivity.java
+++ app/src/main/java/com/baraka/thescout/MainActivity.java
@@ -14,14 +14,19 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.account_question_main) TextView mAskSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        TextView mAskSignUp = findViewById(R.id.account_question_main);
         String text = "Don't have an account? Sign Up";
         SpannableString spannableString = new SpannableString(text);
 
