ec492c0637687dccefde5a108c4cf337f120e550
barakamulas <barakafrank18@gmail.com>
2019-11-10 19:00:23 +0300

    Add a clickable span on the sign up part of the the SignUp textview in main activity and add a show a toast on click

--- app/src/main/java/com/baraka/thescout/MainActivity.java
+++ app/src/main/java/com/baraka/thescout/MainActivity.java
@@ -1,13 +1,18 @@
 package com.baraka.thescout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.text.style.ForegroundColorSpan;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,10 +24,17 @@ public class MainActivity extends AppCompatActivity {
         TextView mAskSignUp = findViewById(R.id.account_question_main);
         String text = "Don't have an account? Sign Up";
         SpannableString ss = new SpannableString(text);
-        ForegroundColorSpan blue = new ForegroundColorSpan(Color.BLUE);
-        ss.setSpan(blue,23,30, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
 
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                Toast.makeText(MainActivity.this,"Sign Up Clicked", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        ss.setSpan(clickableSpan,23,30,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         mAskSignUp.setText(ss);
+        mAskSignUp.setMovementMethod(LinkMovementMethod.getInstance());
 
     }
 }
