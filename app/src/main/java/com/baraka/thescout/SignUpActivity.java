d83721066d2de859f8bf2808e4c070ba76f1ed11
barakamulas <barakafrank18@gmail.com>
2019-11-10 23:29:10 +0300

    Create a clickable span on the Log In option text view in the signup activity and add an intent to lead to login activity on click

--- app/src/main/java/com/baraka/thescout/SignUpActivity.java
+++ app/src/main/java/com/baraka/thescout/SignUpActivity.java
@@ -1,14 +1,48 @@
 package com.baraka.thescout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    @BindView(R.id.account_question_sign_up) TextView mSignUpAccountQuestion;
+    @BindView(R.id.sign_up_button) Button mSignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
+
+        String text = "Already have an account? Log In";
+        SpannableString spannableString = new SpannableString(text);
+
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                Intent intent = new Intent(SignUpActivity.this,LoginActivity.class);
+                Toast.makeText(SignUpActivity.this,"Log In Clicked", Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+            }
+        };
+
+        spannableString.setSpan(clickableSpan,25,31, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        mSignUpAccountQuestion.setText(spannableString);
+        mSignUpAccountQuestion.setMovementMethod(LinkMovementMethod.getInstance());
+
     }
 }
