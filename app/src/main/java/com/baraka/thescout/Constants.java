74f37b96da433efb3ea5130261d1ea28f15af92f
barakamulas <barakafrank18@gmail.com>
2019-11-18 12:16:45 +0300

    Create a constants class tha references football-data.org api credentials

---
+++ app/src/main/java/com/baraka/thescout/Constants.java
@@ -0,0 +1,6 @@
+package com.baraka.thescout;
+
+public class Constants {
+    public static final String COMPETITION_BASE_URL = "http://api.football-data.org/v2/competitions/2021/";
+    public static final String FOOTBALL_DATA_API_KEY = BuildConfig.FOOTBALL_DATA_API_KEY;
+}
