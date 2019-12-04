12e5a977b124ea1eb4169f268b09ee5fcb4762d0
barakamulas <barakafrank18@gmail.com>
2019-12-04 17:25:31 +0300

    Prevent log in when credentials are wrong

--- app/src/main/java/com/baraka/thescout/ui/LoginActivity.java
+++ app/src/main/java/com/baraka/thescout/ui/LoginActivity.java
@@ -118,10 +118,12 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         mAuthProgressDialog.dismiss();
-                        Intent intent = new Intent(LoginActivity.this,DashboardActivity.class);
-                        startActivity(intent);
-                        finish();
-                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                        if(task.isSuccessful()){
+                            Intent intent = new Intent(LoginActivity.this,DashboardActivity.class);
+                            startActivity(intent);
+                            finish();
+                            Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                        }
                         if (!task.isSuccessful()) {
                             Log.w(TAG, "signInWithEmail", task.getException());
                             Toast.makeText(LoginActivity.this, "Authentication failed.",
